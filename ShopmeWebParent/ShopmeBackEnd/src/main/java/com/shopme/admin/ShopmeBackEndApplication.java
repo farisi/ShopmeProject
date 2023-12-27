6ee69c8ada6fd6000762ec205a7ef484c7602b07
salman <salmandriva@gmail.com>
2023-12-27 17:57:33 +0700

    menambah property upload.path

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -1,6 +1,7 @@
 package com.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
@@ -9,7 +10,9 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 public class ShopmeBackEndApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopmeBackEndApplication.class, args);
+		//SpringApplication.run(ShopmeBackEndApplication.class, args);
+		SpringApplication application = new SpringApplication(ShopmeBackEndApplication.class);
+        application.setBannerMode(Banner.Mode.LOG);
+        application.run(args);
 	}
-
 }
