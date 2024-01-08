7652ed023978257e8f2bfbdc4e58599347648409
salman <salmandriva@gmail.com>
2024-01-08 13:33:22 +0700

    menambahkan package com.shopme.site.configures

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/site/configures/WebConfig.java
@@ -0,0 +1,18 @@
+package com.shopme.site.configures;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	public void addViewController(ViewControllerRegistry registry) {
+		registry.addViewController("/{spring:\\w+}")
+			.setViewName("forward:/");
+		registry.addViewController("/**/{spring:\\w+}")
+        	.setViewName("forward:/");
+		registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+        	.setViewName("forward:/");
+	}
+}
